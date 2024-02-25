d86e738e9b039cb006dc425094da3ddb9af609c0
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-02-25 12:45:33 +0700

    feat: initial with unocss

---
+++ src/components/meteors.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const props = defineProps<{ qty?: number; class?: string }>()
+
+const meteors = new Array(props.qty || 20).fill(true)
+</script>
+
+<template>
+  <span v-for="_, idx of meteors" :key="idx" :style="{
+    top: '0px',
+    left: `${Math.floor(Math.random() * (400 - -400) + -400)}px`,
+    animationDelay: `${Math.random() * (0.8 - 0.2) + 0.2}s`,
+    animationDuration: `${Math.floor(Math.random() * (10 - 2) + 2)}s`,
+  }"
+    class="absolute left-1/2 top-1/2 h-.5 rotate-[215deg] meteor-effect rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10] before:absolute before:top-1/2 before:h-[1px] before:w-[50px] before:-translate-y-[50%] before:transform before:bg-gradient-to-r before:from-[#64748b] before:to-transparent before:content-['']">
+
+  </span>
+</template>
+
+<style scoped>
+@keyframes meteor {
+  0% {
+    transform: rotate(215deg) translate(0);
+    opacity: 1;
+  }
+
+  70% {
+    opacity: 1;
+  }
+
+  100% {
+    transform: rotate(215deg) translateX(-500px);
+    opacity: 0;
+  }
+}
+
+.meteor-effect {
+  animation: meteor 5s linear infinite;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import "virtual:uno.css"
+import '@unocss/reset/tailwind.css'
+
+import { createApp } from 'vue'
+import { MotionPlugin, } from "@vueuse/motion";
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(MotionPlugin)
+
+app.mount('#app')
